9371bcbac48605b45c4509b0e0d9294a99c9a844
Damian Rzeszot <damian.rzeszot@gmail.com>
2021-11-16 22:54:17 +0100

    Add Mapping

---
+++ Sources/URLSessionMock/Mapping.swift
@@ -0,0 +1,54 @@
+import Foundation
+
+public typealias Matcher = (URLRequest) -> Bool
+public typealias Handler = (Environment) -> Void
+
+struct Mapping {
+  public let matcher: Matcher
+  public let handler: Handler
+  public let identifier: String?
+}
+
+extension Mapping {
+  init(method: String, path: String, handler: @escaping Handler) {
+    let identifier = "\(method) \(path)"
+    let matcher: Matcher = { request in
+      try! request.httpMethod == method && request.url?.path(matches: path) == true
+    }
+
+    self.init(matcher: matcher, handler: handler, identifier: identifier)
+  }
+
+  static func get(path: String, handler: @escaping Handler) -> Mapping {
+    .init(method: "GET", path: path, handler: handler)
+  }
+
+  static func post(path: String, handler: @escaping Handler) -> Mapping {
+    .init(method: "POST", path: path, handler: handler)
+  }
+}
+
+extension URL {
+  func url(matches regexp: NSRegularExpression) -> Bool {
+    matches(what: absoluteString, regexp: regexp)
+  }
+
+  func url(matches pattern: String) throws -> Bool {
+    url(matches: try NSRegularExpression(pattern: pattern, options: []))
+  }
+
+  func path(matches regexp: NSRegularExpression) -> Bool {
+    matches(what: path, regexp: regexp)
+  }
+
+  func path(matches pattern: String) throws -> Bool {
+    path(matches: try NSRegularExpression(pattern: pattern, options: []))
+  }
+
+  // MARK: -
+
+  private func matches(what: String, regexp: NSRegularExpression) -> Bool {
+    let result = regexp.matches(in: what, options: [], range: NSRange(location: 0, length: what.count))
+    return !result.isEmpty
+  }
+}
---
+++ Tests/URLSessionMockTests/MappingTests.swift
@@ -0,0 +1,87 @@
+import XCTest
+@testable import URLSessionMock
+
+final class MappingTests: XCTestCase {
+
+  var env: Environment!
+
+  override func setUp() {
+    env = Environment(request: .get("/"))
+  }
+
+  override func tearDown() {
+    env = nil
+  }
+
+  // MARK: - Mapping
+
+  func test_mapping_init() {
+    let sut = Mapping(matcher: { request in true }, handler: { env in env.delay = 1 }, identifier: "sut-identifier")
+
+    sut.handler(env)
+
+    XCTAssertEqual(sut.identifier, "sut-identifier")
+    XCTAssertTrue(sut.matcher(.get("/")))
+    XCTAssertEqual(env.delay, 1)
+  }
+
+  func test_mapping_get() {
+    let sut: Mapping = .get(path: "/hello") { env in env.delay = 2 }
+
+    sut.handler(env)
+
+    XCTAssertTrue(sut.matcher(.get("/hello")))
+    XCTAssertEqual(sut.identifier, "GET /hello")
+    XCTAssertEqual(env.delay, 2)
+  }
+
+  func test_mapping_post() {
+    let sut: Mapping = .post(path: "/hello") { env in env.delay = 3 }
+
+    sut.handler(env)
+
+    XCTAssertTrue(sut.matcher(.post("/hello")))
+    XCTAssertEqual(sut.identifier, "POST /hello")
+    XCTAssertEqual(env.delay, 3)
+  }
+
+  
+  // MARK: - Match
+
+  func test_path_matches_pattern() throws {
+    let url = URL(string: "https://example.org/absolute")
+
+    XCTAssertNotNil(url)
+    XCTAssertEqual(try url?.path(matches: "^/absolute$"), true)
+  }
+
+  func test_url_matches_pattern() throws {
+    let url = URL(string: "https://example.org/absolute")
+
+    XCTAssertNotNil(url)
+    XCTAssertEqual(try url?.url(matches: "^https://example.org/absolute$"), true)
+  }
+
+  func test_path_matches_regexp() throws {
+    let url = URL(string: "https://example.org/animals/123/details")
+
+    XCTAssertNotNil(url)
+    XCTAssertEqual(try url?.path(matches: "^/animals/[0-9]+/[a-z]+$"), true)
+  }
+}
+
+extension URLRequest {
+  static func get(_ path: String) -> URLRequest {
+    let url = URL(string: "http://example.org\(path)")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "GET"
+    return request
+  }
+
+  static func post(_ path: String) -> URLRequest {
+    let url = URL(string: "http://example.org\(path)")!
+    var request = URLRequest(url: url)
+    request.httpMethod = "POST"
+    return request
+  }
+}
