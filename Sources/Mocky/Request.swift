9ec9e3687c9c69d418b95abb3b1d8290f05cbe07
Damian Rzeszot <damian.rzeszot@gmail.com>
2021-11-17 01:41:53 +0100

    Add support for http body stream

--- Sources/Mocky/Request.swift
+++ Sources/Mocky/Request.swift
@@ -12,7 +12,13 @@ final public class Request {
   }
 
   public var body: Data? {
-    request.httpBody
+    if let data = request.httpBody {
+      return data
+    } else if let stream = request.httpBodyStream {
+      return Data(reading: stream)
+    } else {
+      return nil
+    }
   }
 
   // MARK: -
@@ -20,5 +26,22 @@ final public class Request {
   public func json<T: Decodable>(type: T.Type, using decoder: JSONDecoder = .init()) throws -> T? {
     guard let body = body else { return nil }
     return try decoder.decode(type, from: body)
+  }
+}
+
+private extension Data {
+  init(reading input: InputStream) {
+    self.init()
+
+    input.open()
+
+    let bufferSize = 1024
+    let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+    while input.hasBytesAvailable {
+        let read = input.read(buffer, maxLength: bufferSize)
+        append(buffer, count: read)
+    }
+    buffer.deallocate()
+    input.close()
   }
 }
