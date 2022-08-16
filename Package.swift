c163a682c75b45be220ef6cf09e12784b84049cf
Damian Rzeszot <damian.rzeszot@gmail.com>
2022-08-17 00:43:37 +0200

    Format Package.swift

--- Package.swift
+++ Package.swift
@@ -3,17 +3,30 @@
 import PackageDescription
 
 let package = Package(
-  name: "Mocky",
-  platforms: [
-    .iOS(.v14)
-  ],
-  products: [
-    .library(name: "Mocky", targets: ["Mocky"])
-  ],
-  targets: [
-    .target(name: "Mocky"),
-    .testTarget(name: "MockyTests", dependencies: ["Mocky"], resources: [
-      .copy("Responses")
-    ])
-  ]
+    name: "Mocky",
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: "Mocky",
+            targets: [
+                "Mocky"
+            ]
+        )
+    ],
+    targets: [
+        .target(
+            name: "Mocky"
+        ),
+        .testTarget(
+            name: "MockyTests",
+            dependencies: [
+                "Mocky"
+            ],
+            resources: [
+                .copy("Responses")
+            ]
+        )
+    ]
 )
